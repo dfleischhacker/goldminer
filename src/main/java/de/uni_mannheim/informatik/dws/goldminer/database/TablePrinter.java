dfff2eaba0faf17c289993322d1aff95151f1a45
Daniel Fleischhacker <daniel@informatik.uni-mannheim.de>
2013-02-08 16:07:08 +0100

    Use Java 1.6 compatible way to compare Integer objects

--- src/main/java/de/uni_mannheim/informatik/dws/goldminer/database/TablePrinter.java
+++ src/main/java/de/uni_mannheim/informatik/dws/goldminer/database/TablePrinter.java
@@ -1372,7 +1372,7 @@ public class TablePrinter {
         Collections.sort(classIds, new Comparator<String>() {
             @Override
             public int compare(String o1, String o2) {
-                return Integer.compare(Integer.parseInt(o1), Integer.parseInt(o2));
+                return Integer.valueOf(Integer.parseInt(o1)).compareTo(Integer.valueOf(Integer.parseInt(o2)));
             }
         });
 
