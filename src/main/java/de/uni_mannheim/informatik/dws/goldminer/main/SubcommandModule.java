e026bf6e7b4e216b7d5b2a1db91f5eb84263474e
Daniel Fleischhacker <daniel@informatik.uni-mannheim.de>
2013-02-18 17:01:59 +0100

    Add small documentation to SubcommandModule Interface

--- src/main/java/de/uni_mannheim/informatik/dws/goldminer/main/SubcommandModule.java
+++ src/main/java/de/uni_mannheim/informatik/dws/goldminer/main/SubcommandModule.java
@@ -2,6 +2,9 @@ package de.uni_mannheim.informatik.dws.goldminer.main;
 
 import net.sourceforge.argparse4j.inf.Namespace;
 
+/**
+ * Interface for implementing subcommand modules
+ */
 public interface SubcommandModule {
     public void runSubcommand(Namespace namespace);
 }
